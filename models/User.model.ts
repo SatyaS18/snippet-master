1afb53b330baaec1284dfd34e419be5ed1b6b964
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-11-05 14:48:39 +0530

    add user model

---
+++ models/User.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { model, Schema } from "mongoose";
+import IUser from "../interfaces/models/user";
+
+const userSchema: Schema = new Schema<IUser>(
+  {
+    userId: {
+      required: true,
+      type: String,
+    },
+    clerkUserId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      required: true,
+      type: String,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+userSchema.set("toObject", { getters: true, virtuals: true });
+userSchema.set("toJSON", {
+  virtuals: true,
+  getters: true,
+});
+
+const User = mongoose.models.User || model<IUser>("User", userSchema);
+
+export default User;
