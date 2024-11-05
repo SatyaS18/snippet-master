39454b4de581506b34d1adf52d82b684d50688f6
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-11-05 15:33:14 +0530

    add Add Snippet button

---
+++ components/Topbar/AddSnippetButton.tsx
@@ -0,0 +1,40 @@
+import { AVAILABLE_LANGUAGES } from "@/constants/languages";
+import { useGlobalContext } from "@/context";
+import { SingleNoteType } from "@/interfaces/context";
+
+
+export default function AddSnippetButton() {
+  const {
+    selectedNoteObject: { setSelectedNote },
+    openNoteContentObject: { setOpenNoteContent },
+    isNewNoteObject: { setIsNewNote },
+  } = useGlobalContext();
+
+  const openNewNoteContent = () => {
+    const newSingleNote: SingleNoteType = {
+      id: crypto.randomUUID(),
+      title: "",
+      isFavorite: false,
+      tags: [],
+      description: "",
+      code: "",
+      language: AVAILABLE_LANGUAGES[0].name,
+      isDeleted: false,
+      createdOn: new Date().toISOString(),
+    };
+
+    setIsNewNote((_) => true);
+    setSelectedNote((_) => newSingleNote);
+    setOpenNoteContent((_) => true);
+  };
+
+  return (
+    <div
+      onClick={openNewNoteContent}
+      className="absolute flex gap-2 px-3 rounded-3xl bg-theme p-1 text-[13px] text-white top-[5px] right-[6px] items-center cursor-pointer select-none"
+    >
+      <div className="font-bold">+</div>
+      <div className="max-md:hidden">Snippet</div>
+    </div>
+  );
+}
