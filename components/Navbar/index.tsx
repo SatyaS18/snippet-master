aadb06dcd8b117645b64ef490612d7095e8ab895
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-11-05 15:41:30 +0530

    add navbar

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import Logo from "../Logo";
+import Buttons from "./Buttons";
+
+
+export default function Navbar() {
+  return (
+    <div className="flex m-5 max-sm:mt-9 mx-8 items-center justify-between max-sm:flex-col">
+      <Logo />
+      <Buttons />
+    </div>
+  );
+}
