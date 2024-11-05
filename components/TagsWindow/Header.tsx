8eb2765d8bf203c2f74740ea3a3993ae698b5f82
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-11-05 15:35:35 +0530

    add tags dialog header

---
+++ components/TagsWindow/Header.tsx
@@ -0,0 +1,46 @@
+import { useGlobalContext } from "@/context";
+import { isDarkMode } from "@/lib/utils";
+import { Close, StyleOutlined } from "@mui/icons-material";
+
+function Header() {
+  const {
+    secondarySidebarMenuObject: {
+      secondarySidebarMenu,
+      setSecondarySidebarMenu,
+    },
+    openSidebarObject: { setOpenSidebar },
+    darkModeObject: { darkMode },
+  } = useGlobalContext();
+
+  const isDarkModeEnabled = isDarkMode(darkMode);
+
+  const handleClose = () => {
+    const updatedMenu = secondarySidebarMenu.map((item) => {
+      return { ...item, isSelected: false };
+    });
+
+    setSecondarySidebarMenu(() => updatedMenu);
+    setOpenSidebar(() => false);
+  };
+
+  return (
+    <div className="flex justify-between items-center">
+      <div
+        className={`flex items-center gap-2 ${
+          isDarkModeEnabled ? "text-white" : "text-black"
+        }`}
+      >
+        <StyleOutlined />
+        <span className="text-md font-bold">Tags</span>
+      </div>
+      <div onClick={() => handleClose()}>
+        <Close
+          sx={{ fontSize: 16 }}
+          className="text-slate-400 cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
