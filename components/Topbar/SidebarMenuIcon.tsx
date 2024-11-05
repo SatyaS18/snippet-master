064d104c5e21f66c0dd80b2e215d96fc722b0188
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-11-05 15:26:31 +0530

    add menu for mobile

---
+++ components/Topbar/SidebarMenuIcon.tsx
@@ -0,0 +1,27 @@
+import { useGlobalContext } from "@/context";
+import { MenuOutlined, CloseOutlined } from "@mui/icons-material";
+
+export default function SidebarMenuIcon() {
+  const {
+    openSidebarObject: { openSidebar, setOpenSidebar },
+    isMobileObject: { isMobile },
+  } = useGlobalContext();
+
+  if (!isMobile) return null;
+
+  return (
+    <>
+      {!openSidebar ? (
+        <MenuOutlined
+          onClick={() => setOpenSidebar((_) => !openSidebar)}
+          className="text-slate-500 cursor-pointer max-md:block hidden"
+        />
+      ) : (
+        <CloseOutlined
+          onClick={() => setOpenSidebar((_) => !openSidebar)}
+          className="text-slate-500 cursor-pointer max-md:block hidden"
+        />
+      )}
+    </>
+  );
+}
