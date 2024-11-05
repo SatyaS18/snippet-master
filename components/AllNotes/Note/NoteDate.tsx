30e6b6781a3eb1a392bcedee6576330059a6709a
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-11-05 16:57:14 +0530

    add note date component

---
+++ components/AllNotes/Note/NoteDate.tsx
@@ -0,0 +1,15 @@
+import { formatDate } from "@/lib/utils";
+
+interface NoteDateProps {
+  date: string;
+}
+
+function NoteDate({ date }: NoteDateProps) {
+  return (
+    <div className="text-slate-500 text-[11px] flex gap-1 font-light mx-4 mt-1">
+      <span className="">{formatDate(date)}</span>
+    </div>
+  );
+}
+
+export default NoteDate;
